01d5f2c6ce1d00c5f1f824560d8f5fc20bb9ec0c
pqr-test <pallavimalli4a3@gmail.com>
2019-11-04 18:49:07 +0530

    commited

---
+++ table2/src/app/table/table.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import { Router } from '@angular/router';
+@Component({
+    selector:"app-table",
+    templateUrl:"table.component.html",
+    styleUrls:["table.component.scss"]
+})
+export class TableComponent implements OnInit {
+users:any;
+constructor(private route:Router){}
+ngOnInit(){
+    var that=this;
+$.ajax({
+    url:"https://jsonplaceholder.typicode.com/users",
+    method:"Get",
+    success:function(res){
+        that.users=res;
+        console.log(that.users);
+      that.gototable(res)
+    }
+})
+}
+gototable(data){
+   this.users=data;
+}
+gotohome($event){
+    this.route.navigate(['form/:id']);
+}
+}
